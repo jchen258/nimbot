870cc76b0d755a5d25a35331fcbd58c032826519
Jason Chen <j.chen2303@gmail.com>
2021-08-06 14:27:46 -0400

    altered code to mention user when prompted

--- main.js
+++ main.js
@@ -8,11 +8,28 @@ client.on('ready', () => {
 
 client.on('message', (msg) => {
   if (msg.content.includes('rach')) {
-    msg.channel.send('Rachel sux');
+    // rachel's user id
+    msg.channel.send('<@330549774434566156> sux');
   }
   if (msg.content.includes('jas')) {
     msg.channel.send('Jason is da bes');
   }
+  if (msg.content.includes('dav')) {
+    // david's user id
+    msg.channel.send('<@691723931736080385> is a wimpy wimp');
+  }
+  if (msg.content.includes('suj')) {
+    // sujung's user id
+    msg.channel.send('<@497640446328307713> is sus');
+  }
+  if (msg.content.includes('scam')) {
+    // semi's user id
+    msg.channel.send('<@113515798785507328> semi is skemi');
+  }
+});
+
+client.on('interactionCreate', (interaction) => {
+  console.log(interaction);
 });
 
 client.login(process.env.TOKEN);
